7a1bfca8ca5dea1831add7667ab27d7d516680db
Sheys12 <IT24104277@my.sliit.lk>
2024-11-22 11:48:51 +0530

    Submitting Lab4Q3

---
+++ Q3/IT24104277Lab4Q3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+   public class IT24104277Lab4Q3
+   {
+         public static void main(String[] args)
+		 {
+		            Scanner input = new Scanner(System.in);
+				    System.out.print("Enter a number: ");
+					int number = input.nextInt();
+					String message = (number == 0)? "The number is zero" : (number>0)? "The number is Positive" : "The number is Negative";
+					System.out.print(message);
+		 }
+  }		 
